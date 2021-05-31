35c1696b9e52da46d14ac52150c525ef707bf704
makrischaralampos <cmakris@uth.gr>
2021-05-31 22:22:19 +0300

    Updates

---
+++ precache-manifest.39cd20c2fec29d8c4b32a3bdb830f7ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d6585a733bfcdfcb87d0ba4cbed6fa",
+    "url": "/auction/index.html"
+  },
+  {
+    "revision": "4253e19b685053405fe0",
+    "url": "/auction/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "68a7b1fc012d1dc00967",
+    "url": "/auction/static/js/2.bc9d994f.chunk.js"
+  },
+  {
+    "revision": "4253e19b685053405fe0",
+    "url": "/auction/static/js/main.d1c0883b.chunk.js"
+  },
+  {
+    "revision": "a533aa9377584338c75f",
+    "url": "/auction/static/js/runtime-main.3ee3047c.js"
+  }
+]);
